1f07f5e81d5a8c938ff6ae60e4c6308cc76c85f6
blueprelude <fun3378@naver.com>
2025-09-01 14:42:55 +0900

    Create project

---
+++ src/main/java/com/fox/YeowoobyeolApplication.java
@@ -0,0 +1,13 @@
+package com.fox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeowoobyeolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeowoobyeolApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/fox/controller/FoxController.java
@@ -0,0 +1,8 @@
+package com.fox.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FoxController {
+
+}
